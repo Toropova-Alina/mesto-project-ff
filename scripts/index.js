d6501b07f839db80102c276e682c732f465ec099
Alina Toropova <al.litovckina@yandex.ru>
2025-03-23 19:29:27 +0300

    Работа с карточками

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,32 @@
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+    cardElement.querySelector('.card__image').src = cardData.link;
 
-// @todo: Функция удаления карточки
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => deleteCard(cardElement));
+  
+    return cardElement;
+  }
+  
+  // @todo: Функция удаления карточки
+  function deleteCard(cardElement) {
+    cardElement.remove();
+  }
+  
+  // @todo: Вывести карточки на страницу
+  function displayCards() {
+    const allCards = document.querySelector('.places__list');
 
-// @todo: Вывести карточки на страницу
+    for (const cardData of initialCards) {
+        const card = createCard(cardData, deleteCard);
+        allCards.append(card);
+    }
+  }
+
+  displayCards();
